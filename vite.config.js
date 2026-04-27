c1b6a82020b0e4f4f18db7e19bed779099cca888
karume-lab <danielkarume@outlook.com>
2026-04-27 23:05:24 +0300

    style: reformat vite.config.js using consistent indentation and double quotes

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,22 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+import laravel from "laravel-vite-plugin";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/css/admin.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
+	plugins: [
+		laravel({
+			input: [
+				"resources/css/app.css",
+				"resources/css/admin.css",
+				"resources/js/app.js",
+			],
+			refresh: true,
+		}),
+		tailwindcss(),
+	],
+	server: {
+		watch: {
+			ignored: ["**/storage/framework/views/**"],
+		},
+	},
 });
