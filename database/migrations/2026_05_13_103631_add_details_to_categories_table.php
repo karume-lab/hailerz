cd6efa93d6616dd9f0495b2bf3e560782d934f58
karume-lab <danielkarume@outlook.com>
2026-05-13 13:36:48 +0300

    feat: create migration file for categories table details

---
+++ database/migrations/2026_05_13_103631_add_details_to_categories_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function run(): void
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->text('description')->nullable();
+            $table->string('icon')->nullable();
+            $table->string('typical_pricing')->nullable();
+            $table->json('popular_genres')->nullable();
+            $table->string('popular_for')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function rollback(): void
+    {
+        Schema::table('categories', function (Blueprint $table) {
+            $table->dropColumn(['description', 'icon', 'typical_pricing', 'popular_genres', 'popular_for']);
+        });
+    }
+};
