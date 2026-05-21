9c620c7de40744aa3ec056a9d16bb464b34adba6
karume-lab <danielkarume@outlook.com>
2026-05-21 20:40:46 +0300

    feat: add powered-by component to app layout and maintenance page

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -266,7 +266,7 @@
       });
     });
   </script>
-
+  <x-powered-by />
 </body>
 
 </html>
---
+++ resources/views/components/powered-by.blade.php
@@ -0,0 +1,8 @@
+<div class="fixed bottom-0 left-0 right-0 py-2 text-center z-10 pointer-events-none">
+    <p class="opacity-40 text-xs pointer-events-auto inline-block">
+        Powered by 
+        <a href="https://karume.vercel.app" target="_blank" class="text-xs underline text-inherit">
+            karume-lab ↗
+        </a>
+    </p>
+</div>
