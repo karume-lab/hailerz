d1f2f2b6a4693e91a0ac5b65c5936d5ec8fd7fe4
karume-lab <danielkarume@outlook.com>
2026-05-26 21:33:04 +0300

    fix: switch Tawk.to configuration to environment variables

--- resources/views/partials/tawk.blade.php
+++ resources/views/partials/tawk.blade.php
@@ -1,15 +1,13 @@
-@if(config('services.tawk.property_id') && config('services.tawk.widget_id'))
-    <!--Start of Tawk.to Script-->
-    <script type="text/javascript">
-        var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-        (function () {
-            var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-            s1.async = true;
-            s1.src = 'https://embed.tawk.to/{{ config('services.tawk.property_id') }}/{{ config('services.tawk.widget_id') }}';
-            s1.charset = 'UTF-8';
-            s1.setAttribute('crossorigin', '*');
-            s0.parentNode.insertBefore(s1, s0);
-        })();
-    </script>
-    <!--End of Tawk.to Script-->
-@endif
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+    var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+    (function () {
+        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/{{ env('TAWK_PROPERTY_ID') }}/{{ env('TAWK_WIDGET_ID') }}';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+    })();
+</script>
+<!--End of Tawk.to Script-->
